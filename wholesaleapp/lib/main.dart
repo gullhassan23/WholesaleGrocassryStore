135549e92832bcc2f10f55f6ba4e14ca088961bf
faizn194 <71870181+faizn194@users.noreply.github.com>
2024-11-27 21:08:04 +0500

    first commit

--- wholesaleapp/lib/main.dart
+++ wholesaleapp/lib/main.dart
@@ -1,12 +1,7 @@
 import 'package:firebase_core/firebase_core.dart';
-import 'package:flashy_tab_bar2/flashy_tab_bar2.dart';
 import 'package:flutter/material.dart';
-import 'package:wholesaleapp/screens/homeScreen/all_products_screen.dart';
-import 'package:wholesaleapp/screens/homeScreen/categories_screen.dart';
-import 'package:wholesaleapp/screens/homeScreen/home_screen.dart';
-import 'package:wholesaleapp/screens/homeScreen/user_profile_screen.dart';
+import 'package:wholesaleapp/screens/splashScreen/splash_screen.dart';
 
-import 'helper/constant/colors_resource.dart';
 import 'helper/utils/svg_utils.dart';
 
 void main() async {
@@ -22,83 +17,11 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  int _selectedIndex = 0;
-  late Widget _child;
-
-  @override
-  void initState() {
-    _child = HomeScreen();
-    super.initState();
-  }
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        backgroundColor: Colors.pink[50],
-        body: _child,
-        bottomNavigationBar: FlashyTabBar(
-          animationDuration: Duration(milliseconds: 800),
-          animationCurve: Curves.linear,
-          selectedIndex: _selectedIndex,
-          showElevation: true,
-          onItemSelected: (index) => setState(() {
-            _selectedIndex = index;
-            _handleNavigationChange(_selectedIndex);
-          }),
-          items: [
-            FlashyTabBarItem(
-              activeColor: ColorsResource.PRIMARY_COLOR,
-              inactiveColor: Colors.blueGrey,
-              icon: Icon(Icons.home),
-              title: Text('Home'),
-            ),
-            FlashyTabBarItem(
-              activeColor: ColorsResource.PRIMARY_COLOR,
-              inactiveColor: Colors.blueGrey,
-              icon: Icon(Icons.category_rounded),
-              title: Text('Categories'),
-            ),
-            FlashyTabBarItem(
-              activeColor: ColorsResource.PRIMARY_COLOR,
-              inactiveColor: Colors.blueGrey,
-              icon: Icon(Icons.fastfood_rounded),
-              title: Text('Products'),
-            ),
-            FlashyTabBarItem(
-              activeColor: ColorsResource.PRIMARY_COLOR,
-              inactiveColor: Colors.blueGrey,
-              icon: Icon(Icons.person),
-              title: Text('Profile'),
-            ),
-          ],
-        ),
-      ),
+      debugShowCheckedModeBanner: false,
+      home: SplashScreen(),
     );
-  }
-
-  void _handleNavigationChange(int index) {
-    setState(() {
-      switch (index) {
-        case 0:
-          _child = HomeScreen();
-          break;
-        case 1:
-          _child = CategoriesScreen();
-          break;
-        case 2:
-          _child = AllProductScreen();
-          break;
-        case 3:
-          _child = UserProfileScreen();
-          break;
-      }
-      _child = AnimatedSwitcher(
-        switchInCurve: Curves.easeOut,
-        switchOutCurve: Curves.easeIn,
-        duration: Duration(milliseconds: 500),
-        child: _child,
-      );
-    });
   }
 }
---
+++ wholesaleapp/lib/screens/homeScreen/navigation.dart
@@ -0,0 +1,78 @@
+import 'package:flashy_tab_bar2/flashy_tab_bar2.dart';
+import 'package:flutter/material.dart';
+import 'package:wholesaleapp/screens/homeScreen/home_screen.dart';
+import 'package:wholesaleapp/screens/homeScreen/user_profile_screen.dart';
+
+import 'all_products_screen.dart';
+import 'categories_screen.dart';
+
+class HomeContentScreen extends StatefulWidget {
+  @override
+  _HomeContentScreenState createState() => _HomeContentScreenState();
+}
+
+class _HomeContentScreenState extends State<HomeContentScreen> {
+  int _selectedIndex = 0;
+
+  late Widget _currentScreen;
+
+  final List<Widget> _screens = [
+    HomeScreen(),
+    CategoriesScreen(),
+    AllProductScreen(),
+    UserProfileScreen(),
+  ];
+
+  @override
+  void initState() {
+    super.initState();
+    _currentScreen = _screens[_selectedIndex]; // Initial screen
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: _currentScreen, // Directly use the screen widget here
+      bottomNavigationBar: FlashyTabBar(
+        animationDuration: Duration(milliseconds: 800),
+        animationCurve: Curves.linear,
+        selectedIndex: _selectedIndex,
+        showElevation: true,
+        onItemSelected: (index) {
+          setState(() {
+            _selectedIndex = index;
+            _currentScreen = _screens[
+                _selectedIndex]; // Update screen based on selected index
+          });
+        },
+        items: [
+          FlashyTabBarItem(
+            activeColor: Colors.blue,
+            inactiveColor: Colors.blueGrey,
+            icon: Icon(Icons.home),
+            title: Text('Home'),
+          ),
+          FlashyTabBarItem(
+            activeColor: Colors.blue,
+            inactiveColor: Colors.blueGrey,
+            icon: Icon(Icons.category_rounded),
+            title: Text('Categories'),
+          ),
+          FlashyTabBarItem(
+            activeColor: Colors.blue,
+            inactiveColor: Colors.blueGrey,
+            icon: Icon(Icons.fastfood_rounded),
+            title: Text('Products'),
+          ),
+          FlashyTabBarItem(
+            activeColor: Colors.blue,
+            inactiveColor: Colors.blueGrey,
+            icon: Icon(Icons.person),
+            title: Text('Profile'),
+          ),
+        ],
+      ),
+    );
+  }
+}
