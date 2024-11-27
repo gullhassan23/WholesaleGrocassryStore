ecb99b113fa68612c8cac781b18bf7214d2926c6
faizn194 <71870181+faizn194@users.noreply.github.com>
2024-11-27 21:18:21 +0500

    first commit

--- wholesaleapp/lib/main.dart
+++ wholesaleapp/lib/main.dart
@@ -1,16 +1,8 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:wholesaleapp/screens/splashScreen/splash_screen.dart';
-
-import 'package:wholesaleapp/screens/homeScreen/all_products_screen.dart';
-import 'package:wholesaleapp/screens/homeScreen/categories_screen.dart';
-import 'package:wholesaleapp/screens/homeScreen/home_screen.dart';
-import 'package:wholesaleapp/screens/homeScreen/user_profile_screen.dart';
-
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:get/get.dart';
 import 'package:wholesaleapp/screens/splashScreen/splash_screen.dart';
-
 
 import 'helper/constant/colors_resource.dart';
 import 'helper/utils/svg_utils.dart';
@@ -30,49 +22,6 @@ class MyApp extends StatefulWidget {
 class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
-
-    return MaterialApp(
-      home: Scaffold(
-        backgroundColor: Colors.pink[50],
-        body: _child,
-        bottomNavigationBar: FlashyTabBar(
-          animationDuration: Duration(milliseconds: 800),
-          animationCurve: Curves.linear,
-          selectedIndex: _selectedIndex,
-          showElevation: true,
-          onItemSelected: (index) => setState(() {
-            _selectedIndex = index;
-            _handleNavigationChange(_selectedIndex);
-          }),
-          items: [
-            FlashyTabBarItem(
-              activeColor: ColorsResource.PRIMARY_COLOR,
-              inactiveColor: Colors.blueGrey,
-              icon: Icon(Icons.home),
-              title: Text('Home'),
-            ),
-            FlashyTabBarItem(
-              activeColor: ColorsResource.PRIMARY_COLOR,
-              inactiveColor: Colors.blueGrey,
-              icon: Icon(Icons.category_rounded),
-              title: Text('Categories'),
-            ),
-            FlashyTabBarItem(
-              activeColor: ColorsResource.PRIMARY_COLOR,
-              inactiveColor: Colors.blueGrey,
-              icon: Icon(Icons.fastfood_rounded),
-              title: Text('Products'),
-            ),
-            FlashyTabBarItem(
-              activeColor: ColorsResource.PRIMARY_COLOR,
-              inactiveColor: Colors.blueGrey,
-              icon: Icon(Icons.person),
-              title: Text('Profile'),
-            ),
-          ],
-        ),
-      ),
-
     return ScreenUtilInit(
       designSize: Size(360, 690),
       minTextAdapt: true,
@@ -90,7 +39,6 @@ class _MyAppState extends State<MyApp> {
           home: const SplashScreen(),
         );
       },
-
     );
   }
 }
