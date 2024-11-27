1edce3747b19dda1d7ce3243f865b9843a63f1ea
faizn194 <71870181+faizn194@users.noreply.github.com>
2024-11-28 01:42:35 +0500

    first commit

--- wholesaleapp/windows/flutter/generated_plugins.cmake
+++ wholesaleapp/windows/flutter/generated_plugins.cmake
@@ -3,27 +3,27 @@
 #
 
 list(APPEND FLUTTER_PLUGIN_LIST
-  cloud_firestore
-  file_selector_windows
-  firebase_auth
-  firebase_core
-  firebase_storage
+        cloud_firestore
+        file_selector_windows
+        firebase_auth
+        firebase_core
+        firebase_storage
         permission_handler_windows
-)
+        )
 
 list(APPEND FLUTTER_FFI_PLUGIN_LIST
-)
+        )
 
 set(PLUGIN_BUNDLED_LIBRARIES)
 
-foreach(plugin ${FLUTTER_PLUGIN_LIST})
+foreach (plugin ${FLUTTER_PLUGIN_LIST})
   add_subdirectory(flutter/ephemeral/.plugin_symlinks/${plugin}/windows plugins/${plugin})
   target_link_libraries(${BINARY_NAME} PRIVATE ${plugin}_plugin)
   list(APPEND PLUGIN_BUNDLED_LIBRARIES $<TARGET_FILE:${plugin}_plugin>)
   list(APPEND PLUGIN_BUNDLED_LIBRARIES ${${plugin}_bundled_libraries})
-endforeach(plugin)
+endforeach (plugin)
 
-foreach(ffi_plugin ${FLUTTER_FFI_PLUGIN_LIST})
+foreach (ffi_plugin ${FLUTTER_FFI_PLUGIN_LIST})
   add_subdirectory(flutter/ephemeral/.plugin_symlinks/${ffi_plugin}/windows plugins/${ffi_plugin})
   list(APPEND PLUGIN_BUNDLED_LIBRARIES ${${ffi_plugin}_bundled_libraries})
-endforeach(ffi_plugin)
+endforeach (ffi_plugin)
