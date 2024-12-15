f34434c93d421e5d330b56b3d1078d236c666d09
gullhassan23 <hassanxhan1433@gmail.com>
2024-12-15 15:48:46 +0500

    latest

--- wholesaleapp/lib/widgets/all_product_text.dart
+++ wholesaleapp/lib/widgets/all_product_text.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 
-import '../screens/homeScreen/all_products_screen.dart';
 
 class AllProductText extends StatelessWidget {
   const AllProductText({
@@ -9,55 +8,31 @@ class AllProductText extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return GestureDetector(
-      onTap: () {
-        Navigator.push(
-          context,
-          MaterialPageRoute(builder: (context) => AllProductScreen()),
-        );
-      },
-      child: Row(
-        children: [
-          Expanded(
-            child: Divider(
-              color: Colors.grey.shade400,
-              thickness: 1,
+    return Row(
+      children: [
+        Expanded(
+          child: Divider(
+            color: Colors.grey.shade400,
+            thickness: 1,
+          ),
+        ),
+        Padding(
+          padding: EdgeInsets.symmetric(horizontal: 8.0),
+          child: Text(
+            'All Products',
+            style: TextStyle(
+              fontSize: 20,
+              fontWeight: FontWeight.bold,
             ),
           ),
-          Padding(
-            padding: EdgeInsets.symmetric(horizontal: 8.0),
-            child: Text(
-              'All Products',
-              style: TextStyle(
-                fontSize: 20,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
+        ),
+        Expanded(
+          child: Divider(
+            color: Colors.grey.shade400,
+            thickness: 1,
           ),
-          Expanded(
-            child: Divider(
-              color: Colors.grey.shade400,
-              thickness: 1,
-            ),
-          ),
-        ],
-      ),
-      // child: Row(
-      //   mainAxisAlignment: MainAxisAlignment.spaceBetween,
-      //   children: [
-      //     Text(
-      //       'All Products',
-      //       style: TextStyle(
-      //         fontSize: 20,
-      //         fontWeight: FontWeight.bold,
-      //       ),
-      //     ),
-      //     Icon(
-      //       Icons.arrow_forward_ios_rounded,
-      //       size: 20,
-      //     ),
-      //   ],
-      // ),
+        ),
+      ],
     );
   }
 }
